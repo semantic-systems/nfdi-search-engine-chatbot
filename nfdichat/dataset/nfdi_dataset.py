44d1552220a2c417d9c1d9b77deff2db98704794
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-07-27 14:08:50 +0200

    [add] nfdidataset module and toy dataset

--- nfdichat/dataset/nfdi_dataset.py
+++ nfdichat/dataset/nfdi_dataset.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+from typing import Dict
 
 
 class NFDIDataset:
     def __int__(self):
+        pass
+
+    def fetch(self) -> [str, Dict]:
         pass
--- nfdichat/dataset/toy_dataset.py
+++ nfdichat/dataset/toy_dataset.py
@@ -2,13 +2,19 @@
 """
 A script for creating toy data needed to use the chatbot in demos and testing enviroment.
 """
+from typing import Dict
+
 from nfdichat.common.config.dataset import DatasetConfig
 from nfdichat.common.util import io
 from nfdichat.dataset.nfdi_dataset import NFDIDataset
 
 
 class ToyDataset(NFDIDataset):
-    def __int__(self):
-        super.__init__()
-        self.nfdi_search_data = {}
-        self.toy_data = io.read_json(DatasetConfig.TOY_DATASET_PATH)
+    def __init__(self):
+        pass
+
+    def fetch(self) -> [str, Dict]:
+        toy_data = io.read_json(DatasetConfig.TOY_DATASET_PATH)
+        search_query = toy_data["Search Term"]
+        retrieved_items = toy_data["results"]
+        return search_query, retrieved_items
