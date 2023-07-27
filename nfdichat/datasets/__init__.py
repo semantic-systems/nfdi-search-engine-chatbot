2e2a8a3bed63a92097da3326a6c28a7b7c197543
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-07-27 15:10:32 +0200

    [minor changes]

---
+++ nfdichat/datasets/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+""" Package for the dataset component. """
+from nfdichat.datasets.dataset import NFDIDataset
+from nfdichat.datasets.toy_dataset import ToyDataset
+
+__all__ = ["NFDIDataset", "ToyDataset"]
