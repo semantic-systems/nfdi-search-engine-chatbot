bf27e48f693a7c01039a3236e0fa62617c9fa833
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-07-27 15:27:03 +0200

    [add] strict data structure

---
+++ nfdichat/common/util/data_structure.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Includes help classes useful for encapsulating data.
+"""
+from typing import Any, Dict
+
+
+class StrictDict(dict):
+    """
+    This class prevents adding new keys to the dictionary
+    """
+
+    def __setitem__(self, key: str, value: Any):
+        self.check(key)
+        dict.__setitem__(self, key, value)
+
+    def update(self, __m: Dict[str, Any], **kwargs):
+        for key, value in __m.items():
+            self.check(key)
+
+        for key, value in __m.items():
+            self.__setitem__(key, value)
+
+    def check(self, key: str):
+        if key not in self:
+            raise KeyError("{} is not a legal key of this StrictDict".format(repr(key)))
