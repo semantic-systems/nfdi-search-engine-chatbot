e01acb90ed9b4f7a7bdcc7d1af37907974ef736f
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-08-26 18:00:36 +0200

    [add] finalize chat module

--- nfdichat/chat.py
+++ nfdichat/chat.py
@@ -9,7 +9,8 @@ from langchain.memory import ConversationBufferMemory
 
 from nfdichat.common.config import (dataset_config, llm_config, main_config,
                                     retriever_config)
-from nfdichat.datasets import Dataset, DocumentProcessor
+from nfdichat.datasets import (Dataset, DocumentProcessor,
+                               ToyDatasetDocumentProcessor)
 from nfdichat.llms import VicunaLLM
 from nfdichat.retrievers import Retriever, SVMBasedRetriever
 from nfdichat.template import CHAIN_PROMPT
@@ -22,37 +23,29 @@ class NFDIChatModel:
 
     # dataset: Dataset = dataset_config[dataset_name]['dataset']()
     retriever: Retriever = eval(retriever_config[retriever_name])(
-        document_processor=dataset_config[dataset_name]["document_processor"]
+        document_processor=eval(dataset_config[dataset_name]["document_processor"])()
     )
     llm = eval(llm_config[llm_name]["MODEL"])()
 
-    chat_history = []
+    # chat_history = []
     chatbot_chain = None
 
-    def __new__(cls, docs: Any):
-        retriever_db = cls.retriever.build_retriever(docs=docs)
-        cls.chat_history = []
-        cls.chatbot_chain = ConversationalRetrievalChain.from_chain_type(
-            llm=cls.llm,
+    def set(self, docs: Any):
+        retriever_db = self.retriever.build_retriever(docs=docs)
+        # cls.chat_history = []
+        self.chatbot_chain = ConversationalRetrievalChain.from_llm(
+            llm=self.llm,
             chain_type="stuff",
             retriever=retriever_db,
             memory=ConversationBufferMemory(
                 memory_key="chat_history", return_messages=True
             ),
-            return_source_documents=True,
-            return_generated_question=True,
         )
 
-    # def chat(self, question: str):
-    #
-    #     result = cls.chatbot_chain({"question": question, 'chat_history': self.chat_history})
-    #
-    #     chat_history.extend([(question, result["answer"])])
-    #
-    #     result["answer"]
+    def chat(self, question: str):
+        result = self.chatbot_chain({"question": question})
+        # self.chat_history.extend([(question, result["answer"])])
+        return result["answer"]
 
     def reset(self, docs: Any):
         self.__new__(docs)
-
-
-print(CHAIN_PROMPT)
