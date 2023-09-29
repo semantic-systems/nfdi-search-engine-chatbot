365c009af461b821c67f6258b48c011041f55123
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-09-29 12:49:02 +0200

    [minor fix]

--- nfdichat/chat.py
+++ nfdichat/chat.py
@@ -13,39 +13,34 @@ from nfdichat.datasets import (Dataset, DocumentProcessor,
                                ToyDatasetDocumentProcessor)
 from nfdichat.llms import VicunaLLM
 from nfdichat.retrievers import Retriever, SVMBasedRetriever
-from nfdichat.template import CHAIN_PROMPT
-
+from langchain.llms.base import LLM
 
 class NFDIChatModel:
     dataset_name: str = main_config["DATASET"]
     retriever_name: str = main_config["RETRIEVER"]
     llm_name: str = main_config["LLM"]
 
-    # dataset: Dataset = dataset_config[dataset_name]['dataset']()
     retriever: Retriever = eval(retriever_config[retriever_name])(
         document_processor=eval(dataset_config[dataset_name]["document_processor"])()
     )
-    llm = eval(llm_config[llm_name]["MODEL"])()
+    llm: LLM = eval(llm_config[llm_name]["MODEL"])()
 
-    # chat_history = []
     chatbot_chain = None
 
     def set(self, docs: Any):
         retriever_db = self.retriever.build_retriever(docs=docs)
-        # cls.chat_history = []
         self.chatbot_chain = ConversationalRetrievalChain.from_llm(
             llm=self.llm,
             chain_type="stuff",
             retriever=retriever_db,
             memory=ConversationBufferMemory(
                 memory_key="chat_history", return_messages=True
-            ),
+            )
         )
 
     def chat(self, question: str):
         result = self.chatbot_chain({"question": question})
-        # self.chat_history.extend([(question, result["answer"])])
         return result["answer"]
 
     def reset(self, docs: Any):
-        self.__new__(docs)
+        self.set(docs)
